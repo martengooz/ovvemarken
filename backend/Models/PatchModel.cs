97a7053420d9b82e8242ce9e40f24ced38da22e9
Kristian Nilsson <kristian.nilsson@attentec.se>
2020-04-09 10:36:40 +0200

    ENdpoint for getting info about a patch from db

---
+++ backend/Models/PatchModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ovvemarken_backend.Models
+{
+    public class PatchModel
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageLink { get; set; }
+
+        public string Creator { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+    }
+}
