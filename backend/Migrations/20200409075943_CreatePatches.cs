99394f44d446325a3cef432bd883074ad0338aa0
Kristian Nilsson <kristian.nilsson@attentec.se>
2020-04-09 10:35:50 +0200

    Db context with pathes table

---
+++ backend/Migrations/20200409075943_CreatePatches.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ovvemarken_backend.Migrations
+{
+    public partial class CreatePatches : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patches",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    ImageLink = table.Column<string>(nullable: true),
+                    Creator = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Width = table.Column<int>(nullable: false),
+                    Height = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patches", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patches");
+        }
+    }
+}
