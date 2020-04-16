33093f636f243f365780765f0fed20a79505c461
Kristian Nilsson <kristian.nilsson@attentec.se>
2020-04-16 08:31:23 +0200

    Summary model with conversion from normal model

---
+++ backend/Models/PatchSummaryModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ovvemarken_backend.Models
+{
+    /// <summary>
+    /// Model for the summary of a patch
+    /// </summary>
+    public class PatchSummaryModel
+    {
+        /// <summary>
+        /// ID of the patch
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Name of the patch
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Link to the images of the patch
+        /// </summary>
+        public string ImageLink { get; set; }
+    }
+}
