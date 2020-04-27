0b0e83b2fafac5ef27535ee7b84d7ed7d22f417a
Mårten Grönlund <marten.gronlund@attentec.se>
2020-04-27 10:10:29 +0200

    Edit webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,40 +1,48 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: [
     // converted entry to an array
     // to allow me to unshift the client later
-    path.resolve(__dirname, './src/index.js')
+    path.resolve(__dirname, "./src/index.js"),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader"
+        use: "babel-loader",
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
   },
   devServer: {
-    port: 3000
+    headers: { "Access-Control-Allow-Origin": "*" },
+    host: "0.0.0.0",
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://localhost:44394",
+        secure: false,
+      },
+    },
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
-      filename: "./index.html"
-    })
-  ]
+      filename: "./index.html",
+    }),
+  ],
 };
